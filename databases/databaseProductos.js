e996b5dc73918219c712afb1600027d997b500ca
DankoLucic <dzlucic@miuandes.cl>
2022-05-14 19:09:41 -0400

    entrega desafío clase 8 API RESTful V2

--- databases/databaseProductos.js
+++ databases/databaseProductos.js
@@ -1,35 +1,54 @@
-const productos = [
-    {
-        id: 1,
-        tittle: "titulo_1",
-        price: 100,
-        thumbnail: "URL_1"
-    },
-    {
-        id: 2,
-        tittle: "titulo_2",
-        price: 200,
-        thumbnail: "URL_2"
-    },
-    {
-        id: 3,
-        tittle: "titulo_3",
-        price: 300,
-        thumbnail: "URL_3"
+
+const fs = require("fs");
+
+// const productos = [
+//     {
+//         id: 1,
+//         tittle: "titulo_1",
+//         price: 100,
+//         thumbnail: "URL_1"
+//     },
+//     {
+//         id: 2,
+//         tittle: "titulo_2",
+//         price: 200,
+//         thumbnail: "URL_2"
+//     },
+//     {
+//         id: 3,
+//         tittle: "titulo_3",
+//         price: 300,
+//         thumbnail: "URL_3"
+//     }
+// ];
+
+rutaArchivoPersistencia = "./productos-express-avanzado.txt";
+let data;
+try{
+    data = fs.readFileSync(rutaArchivoPersistencia, "utf-8");
+} catch (err){
+    throw new Error(`Error de lectura: ${err.message}`);
+}
+
+const productos = JSON.parse(data);
+
+function reEscribirArchivo(ArrayDeObjetos){
+    let ArrayDeObjetoString = JSON.stringify(ArrayDeObjetos);
+    try{
+        fs.writeFileSync(rutaArchivoPersistencia, ArrayDeObjetoString,"utf-8");   
+    } catch (err){
+        throw new Error(`Error de escritura: ${err.message}`);
     }
-];
+}
 
 function generarId(){
-    let array = []
+    let array = [];
     for(i=0; i < productos.length; i++){
         array[i] = Number(productos[i].id)
     }
     array.sort((a,b)=>a-b)
     return (array[array.length-1]+1)
 }
-
-
-
 
 const databaseProductos = {
     
@@ -50,31 +69,35 @@ const databaseProductos = {
         const producto = datos;
         producto.id = generarId();
         productos.push(producto);
+        reEscribirArchivo(productos);
         return producto;
     },
+
     borrarProductoSegunId: id => {
         const indiceBuscado = productos.findIndex( producto => producto.id == id);
         if(indiceBuscado == -1){
-            const error = new Error("no existe ID");
+            const error = new Error("no existe ese producto con ese ID");
             error.tipo = "db not found";
             throw error;
         }
         else{
             productos.splice(indiceBuscado,1);
+            reEscribirArchivo(productos);
         }
     },
     reemplazarProductoSegunId: (id, datos) => {
         if(id == datos.id){
             const indiceBuscado = productos.findIndex( producto => producto.id == id);
             if(indiceBuscado == -1){
-                const error = new Error("no existe ID");
+                const error = new Error("no existe ese producto con ese ID");
                 error.tipo = "db not found";
                 throw error;
             }
             else{
                 const producto = datos;
-                producto.id = id;
+                producto.id = Number(id);
                 productos[indiceBuscado] = producto;
+                reEscribirArchivo[productos];
                 return producto;
             }
         }
